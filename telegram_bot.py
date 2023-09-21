7868a419cb05a0a7e7bc91b6a0ae72ff9511ccbd
bzzzbzzzbzz <s.meln1k@ukr.net>
2023-09-21 17:23:02 +0200

    added posting image to script

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,4 +4,5 @@ TOKEN = '1778883378:AAGAVdFnkvgDJET7rOVcUYpppzGFgWJ8aOs'
 
 bot = telegram.Bot(token=f'{TOKEN}')
 
-bot.send_message(chat_id='@spacepics666', text="Sending message")
+bot.send_message(chat_id='@spacepics666', text="First photo")
+bot.send_photo(chat_id='@spacepics666', photo=open('images/nasa1.jpg', 'rb'))
