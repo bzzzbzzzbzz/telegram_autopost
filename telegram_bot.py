67983671046fb788cf89995116590e012db9b093
bzzzbzzzbzz <s.meln1k@ukr.net>
2023-09-21 17:47:45 +0200

    now images randomly posting one in 4 hours

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,14 @@
-import telegram
+import telegram, os, time, random
 
 TOKEN = '1778883378:AAGAVdFnkvgDJET7rOVcUYpppzGFgWJ8aOs'
 
 bot = telegram.Bot(token=f'{TOKEN}')
 
-bot.send_message(chat_id='@spacepics666', text="First photo")
-bot.send_photo(chat_id='@spacepics666', photo=open('images/nasa1.jpg', 'rb'))
+while True:
+    data = list(os.walk('images'))
+    print(data)
+    images = data[0][2]
+    random.shuffle(images)
+    for image in images:
+        bot.send_photo(chat_id='@spacepics666', photo=open(f'images/{image}', 'rb'))
+        time.sleep(14400)
